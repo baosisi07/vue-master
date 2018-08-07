b416daed1e4ef1cca30b70c1e674947344e9d52c
baosisi <1836535938@qq.com>
2018-08-07 10:41:33 +0800

    add dist

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -8,7 +8,7 @@ const config = {
   output: {
     filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, '../dist'),
-    publicPath: '/dist'
+    publicPath: '/vue-master/dist'
   },
   module: {
     rules: [
